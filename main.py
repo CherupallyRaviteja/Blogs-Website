58573b58e702987c510ed2bd45b7c1a8a57d8d41
Ravi Teja <cherupellyraviteja2005@gmail.com>
2025-08-12 21:27:16 +0530

    add local Db-url

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ login_manager.login_view = "login"
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("Database_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("Database_URL","sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
